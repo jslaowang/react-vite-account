cfb21ac74b5738fab31b3ea4ca511604dd829531
张文川 <13522750636@163.com>
2022-03-11 14:32:42 +0800

    feat: 搭建环境

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from 'react'
+import routes from '@/router'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom"
-import routes from './router'
+
+import { ConfigProvider } from 'zarm'
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
+// import 'zarm/dist/zarm.css'
 
 function App() {
   return <Router>
-    <Switch>
-      {
-        routes.map(route =>
-          <Route exact key={route.path} path={route.path}>
-            <route.component />
-          </Route>
-        )
-      }
-    </Switch>
-  </Router >
+    <ConfigProvider primaryColor='#007fff' locale={zhCN}>
+      <Switch>
+        {
+          routes.map(route =>
+            <Route exact key={route.path} path={route.path}>
+              <route.component />
+            </Route>
+          )
+        }
+      </Switch>
+    </ConfigProvider>
+  </ Router>
 }
 
 export default App
